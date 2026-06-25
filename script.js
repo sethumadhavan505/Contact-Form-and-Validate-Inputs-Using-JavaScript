2c8b5af7186ddb47ce61549555aaf1ebef53b673
SETHU MADHAVAN P <211614649+sethumadhavan505@users.noreply.github.com>
2026-06-26 00:29:03 +0530

    Add offer validation to form submission

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ e.preventDefault();
 const name = document.getElementById('name').value;
 const email = document.getElementById('email').value;
 const phone = document.getElementById('phone').value;
+const offer = document.getElementById('offer').value;
 const dept = document.getElementById('dept').value;
 
-if(name === '' || email === '' || phone === '' || dept === ''){
-alert('Please fill all details');
-return;
+if(name === '' || email === '' || phone === '' || offer === '' || dept === ''){
+    alert('Please fill all details');
+    return;
 }
 
-if(phone.length !== 10){
+if (!/^[0-9]{10}$/.test(phone)) {
     alert("Please enter a valid 10-digit mobile number");
     return;
 }
@@ -22,6 +23,7 @@ if(phone.length !== 10){
 localStorage.setItem('odrsName', name);
 localStorage.setItem('odrsEmail', email);
 localStorage.setItem('odrsPhone', phone);
+localStorage.setItem('odrsOffer', offer);
 localStorage.setItem('odrsDept', dept);
 
 window.location.href = 'payment.html';
