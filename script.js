97e11bc49844dcb1c8624a00cf19cc4f67431e0f
SETHU MADHAVAN P <211614649+sethumadhavan505@users.noreply.github.com>
2026-06-25 23:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ e.preventDefault();
 
 const name = document.getElementById('name').value;
 const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
 const dept = document.getElementById('dept').value;
 
-if(name === '' || email === '' || dept === ''){
+if(name === '' || email === '' || phone === '' || dept === ''){
 alert('Please fill all details');
 return;
 }
 
+if(phone.length !== 10){
+    alert("Please enter a valid 10-digit mobile number");
+    return;
+}
+
 localStorage.setItem('odrsName', name);
 localStorage.setItem('odrsEmail', email);
+localStorage.setItem('odrsPhone', phone);
 localStorage.setItem('odrsDept', dept);
 
 window.location.href = 'payment.html';
